9caa8b4e9b254bf15b9529e4916e8d2d77b7645c
HymaSaranya <hymasaranyab@gmail.com>
2023-08-13 01:24:23 -0600

    Login and logout pages added

--- Vkart/urls.py
+++ Vkart/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 from .import views
 from django.conf.urls.static import static
 from django.conf import settings
+from .import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,4 +27,5 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
+    
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
